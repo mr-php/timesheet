496ce11cb0e76880ac06660b74e5655e73a1cdb0
cornernote <cornernote@gmail.com>
2017-04-18 20:53:47 +0930

    allow local config using json base64 env

--- src/config/common.php
+++ src/config/common.php
@@ -1,35 +1,6 @@
 <?php
 
 // Basic configuration, used in web and console applications
-
-date_default_timezone_set(getenv('APP_TIMEZONE'));
-
-function debug($var, $name = null, $attributesOnly = true)
-{
-    $bt = debug_backtrace();
-    $file = str_ireplace(dirname(dirname(__FILE__)), '', $bt[0]['file']);
-    if (!class_exists('\yii\db\BaseActiveRecord', false))
-        $attributesOnly = false;
-    $name = $name ? '<b><span style="font-size:18px;">' . $name . ($attributesOnly ? ' [attributes]' : '') . '</span></b>:<br/>' : '';
-    echo '<div style="background: #FFFBD6">';
-    echo '<span style="font-size:12px;">' . $name . ' ' . $file . ' on line ' . $bt[0]['line'] . '</span>';
-    echo '<div style="border:1px solid #000;">';
-    echo '<pre>';
-    if (is_scalar($var)) {
-        var_dump($var);
-    } elseif ($attributesOnly && $var instanceof \yii\db\BaseActiveRecord) {
-        print_r($var->attributes);
-    } elseif ($attributesOnly && is_array($var) && current($var) instanceof \yii\db\BaseActiveRecord) {
-        foreach ($var as $k => $_var) {
-            $var[$k] = $_var->attributes;
-        }
-        print_r($var);
-    } else {
-        print_r($var);
-    }
-    echo '</pre></div></div>';
-}
-
 return [
     'id' => getenv('APP_NAME'),
     'name' => getenv('APP_TITLE'),
--- src/config/main.php
+++ src/config/main.php
@@ -1,11 +1,41 @@
 <?php
 
+use yii\helpers\Json;
+
+date_default_timezone_set(getenv('APP_TIMEZONE'));
+
+function debug($var, $name = null, $attributesOnly = true)
+{
+    $bt = debug_backtrace();
+    $file = str_ireplace(dirname(dirname(__FILE__)), '', $bt[0]['file']);
+    if (!class_exists('\yii\db\BaseActiveRecord', false))
+        $attributesOnly = false;
+    $name = $name ? '<b><span style="font-size:18px;">' . $name . ($attributesOnly ? ' [attributes]' : '') . '</span></b>:<br/>' : '';
+    echo '<div style="background: #FFFBD6">';
+    echo '<span style="font-size:12px;">' . $name . ' ' . $file . ' on line ' . $bt[0]['line'] . '</span>';
+    echo '<div style="border:1px solid #000;">';
+    echo '<pre>';
+    if (is_scalar($var)) {
+        var_dump($var);
+    } elseif ($attributesOnly && $var instanceof \yii\db\BaseActiveRecord) {
+        print_r($var->attributes);
+    } elseif ($attributesOnly && is_array($var) && current($var) instanceof \yii\db\BaseActiveRecord) {
+        foreach ($var as $k => $_var) {
+            $var[$k] = $_var->attributes;
+        }
+        print_r($var);
+    } else {
+        print_r($var);
+    }
+    echo '</pre></div></div>';
+}
+
 // Define application aliases
-Yii::setAlias('@app', dirname(__DIR__).'/..');
+Yii::setAlias('@app', dirname(__DIR__) . '/..');
 Yii::setAlias('@root', '@app/..');
-Yii::setAlias('@runtime', dirname(__DIR__).'/../../runtime');
-Yii::setAlias('@web', dirname(__DIR__).'/../web');
-Yii::setAlias('@webroot', dirname(__DIR__).'/web');
+Yii::setAlias('@runtime', dirname(__DIR__) . '/../../runtime');
+Yii::setAlias('@web', dirname(__DIR__) . '/../web');
+Yii::setAlias('@webroot', dirname(__DIR__) . '/web');
 
 // Load $merge configuration files
 $applicationType = php_sapi_name() == 'cli' ? 'console' : 'web';
@@ -17,5 +47,6 @@ return \yii\helpers\ArrayHelper::merge(
     require("{$configDir}/{$applicationType}.php"),
     (file_exists("{$configDir}/common-{$env}.php")) ? require("{$configDir}/common-{$env}.php") : [],
     (file_exists("{$configDir}/{$applicationType}-{$env}.php")) ? require("{$configDir}/{$applicationType}-{$env}.php") : [],
-    (file_exists(getenv('APP_CONFIG_FILE'))) ? require(getenv('APP_CONFIG_FILE')) : []
+    (file_exists(getenv('APP_CONFIG_FILE'))) ? require(getenv('APP_CONFIG_FILE')) : [],
+    getenv('APP_CONFIG_JSON_BASE64') ? Json::decode(base64_decode(getenv('APP_CONFIG_JSON_BASE64'))) : []
 );
