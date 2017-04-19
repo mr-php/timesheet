f12064449337294515fccace266ad161bdded651
cornernote <cornernote@gmail.com>
2017-04-20 02:12:01 +0930

    added migration paths

--- src/config/console.php
+++ src/config/console.php
@@ -2,4 +2,17 @@
 
 return [
     'controllerNamespace' => 'app\commands',
+    'params' => [
+        'yii.migrations' => [
+            //'@yii/rbac/migrations',
+            //'@dektrium/user/migrations',
+            //'@bedezign/yii2/audit/migrations',
+            '@vendor/yii2mod/yii2-settings/migrations',
+        ],
+    ],
+    'controllerMap' => [
+        'migrate' => [
+            'class' => '\dmstr\console\controllers\MigrateController',
+        ],
+    ],
 ];
