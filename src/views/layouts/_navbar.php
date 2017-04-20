c29e173af2ffff0afc8e0a4e4f394114fef354c6
cornernote <cornernote@gmail.com>
2017-04-21 02:26:45 +0930

    added kickass link

--- src/views/layouts/_navbar.php
+++ src/views/layouts/_navbar.php
@@ -88,6 +88,10 @@ echo Nav::widget([
                         'target' => '_blank',
                     ],
                 ],
+                [
+                    'label' => Yii::t('app', 'KickAss!'),
+                    'url' => "javascript:var KICKASSVERSION='2.0';var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//hi.kickassapp.com/kickass.js';void(0);",
+                ],
             ],
         ],
     ],
