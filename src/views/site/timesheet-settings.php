f2e6379a2520ee7313ec3c1d69bffa0a8d9362b4
cornernote <cornernote@gmail.com>
2017-04-21 01:59:54 +0930

    update example settings

--- src/views/site/timesheet-settings.php
+++ src/views/site/timesheet-settings.php
@@ -12,8 +12,11 @@ $this->title = Yii::t('app', 'TimeSheet Settings');
 
 <?php echo $form->field($model, 'staff')->textarea()->hint(Yii::t('app', 'Example:') . Html::tag('pre', \yii\helpers\Json::encode([
         'admin' => [
-            'name' => 'Admin',
-            'toggl_api_key' => '817212b50437a8531fdac89bc22e7dc8',
+            'name' => 'Test',
+            'email' => 'timesheet-staff@mailinator.com',
+            'toggl_api_key' => 'a67aaf4b789d150863f5f2b6583fb4ff',
+            'saasu_contact_uid' => 1234567890,
+            'saasu_tax_code' => 'G11,G14', // G11 = inc gst
             'sell' => 100,
             'cost' => 50,
             'multiplier' => 1,
@@ -31,9 +34,9 @@ $this->title = Yii::t('app', 'TimeSheet Settings');
 <?php echo $form->field($model, 'projects')->textarea()->hint(Yii::t('app', 'Example:') . Html::tag('pre', \yii\helpers\Json::encode([
         'none' => [
             'name' => 'No Project',
-            'email' => 'test@mailinator.com',
-            'saasu_contact_uid' => 221813,
-            'saasu_tax_code' => 'G1', // 'G1,G2',
+            'email' => 'timesheet-project@mailinator.com',
+            'saasu_contact_uid' => 1234567890,
+            'saasu_tax_code' => 'G1,G2', // G1 = inc gst
             'tax_rate' => 0.1,
             'base_rate' => 150,
             'base_hours' => 2,
