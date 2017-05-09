c17c1060abf0989ac5eda3423876e658420ca809
cornernote <cornernote@gmail.com>
2017-05-09 20:55:21 +0930

    added more zipbooks code, still incomplete

--- src/components/ZipBooks.php
+++ src/components/ZipBooks.php
@@ -56,11 +56,39 @@ class ZipBooks extends Component
     /**
      * @param string $pid
      * @param array $times
-     * @throws Exception
      */
     public function createInvoice($pid, $times)
     {
-        // todo
+        /** @see https://developer.zipbooks.com/#api-Invoices-PostInvoice */
+        $invoice = [
+            'customer' => '',
+            'number' => '',
+            'date' => '',
+            'discount' => '',
+            'accept_credit_cards' => '',
+            'terms' => '',
+            'notes' => '',
+            'logo_filename' => '',
+            'lineItems' => [],
+        ];
+        $i = 1;
+        $invoice['lineItems'][$i] = [
+            'type' => '',
+            'name' => '',
+            'notes' => '',
+            'rate' => '',
+            'quantity' => '',
+        ];
+        $this->_call('invoices', $invoice);
+
+        /** @see https://developer.zipbooks.com/#api-Invoices-postInvoiceSend */
+        $this->_call('invoices/:id/send', [
+            'send_to' => '',
+            'subject' => '',
+            'message' => '',
+            'bcc' => true,
+            'pdf' => true,
+        ]);
     }
 
     /**
@@ -70,7 +98,48 @@ class ZipBooks extends Component
      */
     public function createExpense($sid, $times)
     {
-        // todo
+        /** @see https://developer.zipbooks.com/#api-Expenses-PostExpense */
+        $this->_call('expenses', [
+            'amount' => '',
+            'date' => '',
+            'customer_id' => '',
+            'name' => '',
+            'category' => '',
+            'note' => '',
+            'image_filename' => '',
+        ]);
+    }
+
+    /**
+     * @param $action
+     * @param array $params
+     * @return mixed
+     */
+    private function _call($action, $params = [])
+    {
+        // https://developer.zipbooks.com/#api-Authentication-PostAuthLogin
+        //https://api.zipbooks.com/v1/auth/login
+        $client = new \GuzzleHttp\Client();
+
+        if (!$this->token) {
+            $response = $client->request('POST', 'https://api.zipbooks.com/v1/auth/login', [
+                'json' => ['email' => $this->email, 'password' => $this->password],
+            ])->getBody()->getContents();
+            $this->token = $response->token;
+        }
+
+        if ($this->token) {
+            $response = $client->request('POST', 'https://api.zipbooks.com/v1/auth/check')->getBody()->getContents();
+        }
+
+        $url = 'https://api.zipbooks.com/v1/' . $action;
+        return $client->request('POST', $url, [
+            'json' => $params,
+            'auth' => ['user', 'pass']
+
+        ], [
+            'headers' => ['Authorization' => 'Bearer ' . $this->token],
+        ])->getBody()->getContents();
     }
 
 }
